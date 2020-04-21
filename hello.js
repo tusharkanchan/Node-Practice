635a6b4bd28dcfbdc09ffa407de592fd41d51d80
tusharkanchan <melayertush@gmail.com>
2020-04-22 01:11:28 +0530

    read file data

--- hello.js
+++ hello.js
@@ -1,16 +1,21 @@
 const http = require('http');
 const cd = require('./customDate');
 const url = require('url');
+const fs= require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('hello current date and time is' + cd.myDateTime())
-    let q = url.parse(req.url, true).query;
-    let txt = q.year + '' + q.month;
-    res.write(txt)
-    res.end();
+    fs.readFile('fileSystem.html',(err,data)=>{
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+    // res.write('hello current date and time is' + cd.myDateTime())
+    // let q = url.parse(req.url, true).query;
+    // let txt = q.year + '' + q.month;
+    // res.write(txt)
+    // res.end();
 })
 
 
